f27ab51db4c92b71159791c0948ba01d0220f5bb
Mekha <mekhajaimon2005@gmail.com>
2026-05-29 23:54:35 +0530

    Add GitHub Actions CI pipelines

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 
 from flask import Flask, render_template, request, redirect, session, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_mysqldb import MySQL
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
+limiter = Limiter(
+    key_func=get_remote_address,
+    app=app,
+    default_limits=["10 per minute"]
+)
+
+app.secret_key = 'secret123'
+
+# MySQL Configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'mekha@2005'
+app.config['MYSQL_DB'] = 'student_notes_db'
+
+mysql = MySQL(app)
 app.secret_key = 'studentnotessecret'
 
 # Temporary notes storage
@@ -18,20 +37,58 @@ def login():
 
     if request.method == 'POST':
 
-        session['loggedin'] = True
-        session['email'] = request.form['email']
+        email = request.form['email']
+        password = request.form['password']
 
-        flash('Login Successful!')
+        cur = mysql.connection.cursor()
 
-        return redirect('/dashboard')
+        cur.execute(
+            "SELECT * FROM users WHERE email=%s",
+            (email,)
+        )
+
+        user = cur.fetchone()
+
+        cur.close()
+
+        if user and check_password_hash(user[3], password):
+
+            session['loggedin'] = True
+            session['email'] = user[2]
+
+            flash('Login Successful!')
+
+            return redirect('/dashboard')
+
+        else:
+            return "Invalid Email or Password"
 
     return render_template('login.html')
+
 
 # Register Page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 
     if request.method == 'POST':
+
+        fullname = request.form['fullname']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Hash Password
+        hashed_password = generate_password_hash(password)
+
+        cur = mysql.connection.cursor()
+
+        cur.execute(
+            "INSERT INTO users(fullname,email,password) VALUES(%s,%s,%s)",
+            (fullname, email, hashed_password)
+        )
+
+        mysql.connection.commit()
+
+        cur.close()
 
         flash('Registration Successful! Please Login.')
 
@@ -117,3 +174,12 @@ def logout():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+@app.after_request
+def add_security_headers(response):
+
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['X-Frame-Options'] = 'DENY'
+    response.headers['X-XSS-Protection'] = '1; mode=block'
+
+    return response
